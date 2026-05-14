c998726e62fd6e4f46c83f15db6e3d54e32da4cf
havario <157877551+havario@users.noreply.github.com>
2026-05-14 15:43:03 +0800

    License
    Signed-off-by: havario <157877551+havario@users.noreply.github.com>

--- tools/build.sh
+++ tools/build.sh
@@ -1,4 +1,6 @@
 #!/usr/bin/env bash
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2026 honeok <i@honeok.com>
 
 set -Eeuo pipefail
 
--- tools/render.py
+++ tools/render.py
@@ -1,4 +1,6 @@
 #!/usr/bin/env python3
+# SPDX-License-Identifier: MIT
+# Copyright (c) 2026 honeok <i@honeok.com>
 
 import json
 import mimetypes
